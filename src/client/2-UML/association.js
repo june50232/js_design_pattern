4c1c14b32ef86ec3717a6693f6df9addb1f87de1
june.chiu <s982535@gmail.com>
2021-07-17 15:31:37 +0800

    2.12 UML association
    https://www.youtube.com/watch?v=Iq3Ea-1foy8&t=177s&ab_channel=JomyKing

--- src/client/2-UML/association.js
+++ src/client/2-UML/association.js
@@ -7,3 +7,40 @@ class People {
 
     }
 }
+
+class A extends People {
+    constructor(name, house) {
+        super(name, house)
+        this.name = name
+        this.city = house.city || ''
+    }
+    saySomething() {
+        console.log(`I am ${this.name}, settled in ${this.city}`)
+    }
+}
+
+class B extends People {
+    constructor(name, house) {
+        super(name, house)
+        this.name = name
+        this.city = house.city || ''
+    }
+    saySomething() {
+        console.log(`I am ${this.name}, settled in ${this.city}`)
+    }
+}
+
+class House {
+    constructor(city) {
+        this.city = city
+    }
+    showCity() {
+        console.log(`city is ${this.city}`)
+    }
+}
+
+let aHouse = new House('Taipei')
+let a = new A('A', aHouse)
+a.saySomething()
+let b = new B('B', aHouse)
+b.saySomething()
--- src/client/main.js
+++ src/client/main.js
@@ -2,4 +2,5 @@ require("./main.scss");
 // require("./1-objectOrientation/object.js")
 // require("./1-objectOrientation/inheritance.js")
 // require("./1-objectOrientation/polymorphism.js")
-require("./1-objectOrientation/example.js")
+// require("./1-objectOrientation/example.js")
+require("./2-UML/association.js")
